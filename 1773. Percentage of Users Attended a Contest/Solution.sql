d7de2d5f4e0ca0a284296dc558dc2bdb01f5a6a2
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2024-12-14 02:59:42 -0500

    MySQL Solution
            Submission Statistics:
            Question #: 1773
            Language: MySQL
            Runtime: 1459 ms
            Runtime percentile: 14.62
            Memory: 0 MB
            Memory percentile: 100.00

--- 1773. Percentage of Users Attended a Contest/Solution.sql
+++ 1773. Percentage of Users Attended a Contest/Solution.sql
@@ -1,10 +1,4 @@
-SELECT
-    r.contest_id,
-    ROUND(COUNT(DISTINCT r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
-FROM
-    Register r
-GROUP BY
-    r.contest_id
-ORDER BY
-    percentage DESC,
-    r.contest_id ASC;
+SELECT contest_id, round(COUNT(*)*100/(select count(*) from Users),2) AS percentage FROM Register r
+JOIN Users u USING(user_id)
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id ASC
