5a9f391e8dfbfc193d7f79b47cdd7e5eed7c3d6a
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2024-12-14 02:42:50 -0500

    MySQL Solution
            Submission Statistics:
            Question #: 1390
            Language: MySQL
            Runtime: 1139 ms
            Runtime percentile: 10.88
            Memory: 0 MB
            Memory percentile: 100.00

--- 1390. Average Selling Price/Solution.sql
+++ 1390. Average Selling Price/Solution.sql
@@ -1,3 +1,3 @@
-SELECT p.product_id,ifnull(round(SUM(units*price)/sum(units),2),0) AS average_price FROM Prices p
+SELECT p.product_id,coalesce(round(SUM(units*price)/sum(units),2),0) AS average_price FROM Prices p
 LEFT JOIN UnitsSold u on u.product_id=p.product_id and u.purchase_date between p.start_date and p.end_date
 GROUP BY product_id
