ee2c72422b1df57798d7861c3521efa33bfc7686
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2024-12-16 00:25:42 -0500

    MySQL Solution
            Submission Statistics:
            Question #: 1155
            Language: MySQL
            Runtime: 2252 ms
            Runtime percentile: 5.00
            Memory: 0 MB
            Memory percentile: 100.00

--- 1155. Product Sales Analysis III/Solution.sql
+++ 1155. Product Sales Analysis III/Solution.sql
@@ -1,7 +1,6 @@
-SELECT product_id, year AS first_year, quantity, price
+SELECT product_id, year as first_year, quantity,price 
 FROM Sales
-WHERE (product_id, year) IN (
-    SELECT product_id, MIN(year)
-    FROM Sales
+WHERE (product_id,year) IN(
+    SELECT product_id,MIN(year) from Sales
     GROUP BY product_id
-);
+)
