c79f8d165152dbd9bcd44423c3a68ced51558750
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2024-12-23 22:25:05 -0500

    MySQL Solution
            Submission Statistics:
            Question #: 2087
            Language: MySQL
            Runtime: 1113 ms
            Runtime percentile: 15.01
            Memory: 0 MB
            Memory percentile: 100.00

--- 2087. Confirmation Rate/Solution.sql
+++ 2087. Confirmation Rate/Solution.sql
@@ -1,3 +1,4 @@
+# Write your MySQL query statement below
 SELECT s.user_id , ROUND(COUNT(CASE WHEN action='confirmed' THEN 1 END)/COUNT(*),2) AS confirmation_rate FROM Signups s
 LEFT JOIN Confirmations c USING(user_id)
 GROUP BY s.user_id
