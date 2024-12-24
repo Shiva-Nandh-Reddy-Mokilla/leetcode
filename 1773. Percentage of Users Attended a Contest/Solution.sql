8135b279615912ddc367cb7b73ebeb9cfa6d8943
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2024-12-23 22:24:35 -0500

    MySQL Solution
            Submission Statistics:
            Question #: 1773
            Language: MySQL
            Runtime: 999 ms
            Runtime percentile: 73.05
            Memory: 0 MB
            Memory percentile: 100.00

--- 1773. Percentage of Users Attended a Contest/Solution.sql
+++ 1773. Percentage of Users Attended a Contest/Solution.sql
@@ -1,3 +1,4 @@
+# Write your MySQL query statement below
 SELECT contest_id, round(COUNT(*)*100/(select count(*) from Users),2) AS percentage FROM Register r
 JOIN Users u USING(user_id)
 GROUP BY contest_id
