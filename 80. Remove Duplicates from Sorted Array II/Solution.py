d5e8058c29ea31b3e1b36885daa1d2d97cee80a3
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-03-15 14:11:08 -0400

    Python Solution
          Submission Statistics:
          Question #: 80
          Language: Python
          Runtime: 73 ms
          Runtime percentile: 32.82
          Memory: 15.388 MB
          Memory percentile: 40.10

---
+++ 80. Remove Duplicates from Sorted Array II/Solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        nums.sort()
+        ij=1
+        count=1
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1]:
+                count+=1
+            else:
+                count=1
+            if count<=2:
+                nums[ij]=nums[i]
+                ij+=1
+        
+        return ij 
