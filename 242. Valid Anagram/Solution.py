f5d95788fa4e70cfa6fc48361f96ff278732e0de
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-03-23 18:08:15 -0400

    Python Solution
          Submission Statistics:
          Question #: 242
          Language: Python
          Runtime: 24 ms
          Runtime percentile: 40.37
          Memory: 14.46 MB
          Memory percentile: 10.63

--- 242. Valid Anagram/Solution.py
+++ 242. Valid Anagram/Solution.py
@@ -5,5 +5,15 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return Counter(s)==Counter(t)
-        
+        if len(s)!=len(t):
+            return False
+
+        counts,countt={},{}
+
+        for i in range(len(s)):
+            counts[s[i]] = 1 + counts.get(s[i],0)
+            countt[t[i]] = 1 + countt.get(t[i],0)
+
+        return counts==countt
+
+
