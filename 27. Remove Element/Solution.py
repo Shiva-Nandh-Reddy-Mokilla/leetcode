867cd4e5a8724d826b476161fc13e289464210f2
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-03-24 15:09:28 -0400

    Python Solution
          Submission Statistics:
          Question #: 27
          Language: Python
          Runtime: 1 ms
          Runtime percentile: 11.55
          Memory: 12.464 MB
          Memory percentile: 51.67

--- 27. Remove Element/Solution.py
+++ 27. Remove Element/Solution.py
@@ -6,9 +6,10 @@ class Solution(object):
         :rtype: int
         """
         i=0
-        while i < len(nums):
-            if val==nums[i]:
+        while i<len(nums):
+            if nums[i]==val:
                 nums.pop(i)
             else:
                 i+=1
         return len(nums)
+
