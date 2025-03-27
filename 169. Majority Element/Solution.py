23891f1a76a7951aebe981909d27666505c5d591
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-03-27 15:01:52 -0400

    Python Solution
          Submission Statistics:
          Question #: 169
          Language: Python
          Runtime: 3 ms
          Runtime percentile: 86.10
          Memory: 13.712 MB
          Memory percentile: 31.50

--- 169. Majority Element/Solution.py
+++ 169. Majority Element/Solution.py
@@ -5,16 +5,7 @@ class Solution(object):
         :rtype: int
         """
         nums.sort()
-        n=len(nums)/2
-        count=1
-        for i in range(1,len(nums)):
-            if nums[i]==nums[i-1]:
-                count+=1
-                
-            else:
-                count=0
-            if count>n:
-                return nums[i]
+        
         return nums[len(nums)//2]
             
             
