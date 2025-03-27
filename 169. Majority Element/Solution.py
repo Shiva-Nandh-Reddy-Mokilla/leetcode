32c9262e79733a0ebe54c4d75fbc9c4bd4d235ca
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-03-27 15:00:33 -0400

    Python Solution
          Submission Statistics:
          Question #: 169
          Language: Python
          Runtime: 11 ms
          Runtime percentile: 49.99
          Memory: 14.536 MB
          Memory percentile: 11.15

--- 169. Majority Element/Solution.py
+++ 169. Majority Element/Solution.py
@@ -4,19 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return 0
-        
         nums.sort()
-      
-        count = 1
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
-                count += 1
+        n=len(nums)/2
+        count=1
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1]:
+                count+=1
+                
             else:
-                count = 1  # Reset count for the new element
-            
-            if count > len(nums) / 2:
+                count=0
+            if count>n:
                 return nums[i]
-        
-        return nums[len(nums) // 2]  # Majority element is always in the middle after sorting
+        return nums[len(nums)//2]
+            
+            
+
