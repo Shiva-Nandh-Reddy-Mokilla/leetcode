3d88714da840e5cf3a54483bb2ab92e52d859ad8
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-03-27 20:45:37 -0400

    Python Solution
          Submission Statistics:
          Question #: 217
          Language: Python
          Runtime: 9 ms
          Runtime percentile: 83.20
          Memory: 26.056 MB
          Memory percentile: 15.66

--- 217. Contains Duplicate/Solution.py
+++ 217. Contains Duplicate/Solution.py
@@ -4,10 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        seen=set()
-        for num in nums:
-            if num in seen:
-                return True
-            seen.add(num)
-        return False
+        return len(set(nums))<len(nums)
         
