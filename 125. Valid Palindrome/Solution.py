9b2bae8fb49711e0784b8113bc0c248760723d4c
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-03-30 15:19:17 -0400

    Python Solution
          Submission Statistics:
          Question #: 125
          Language: Python
          Runtime: 15 ms
          Runtime percentile: 63.79
          Memory: 12.588 MB
          Memory percentile: 99.21

--- 125. Valid Palindrome/Solution.py
+++ 125. Valid Palindrome/Solution.py
@@ -5,21 +5,13 @@ class Solution(object):
         :rtype: bool
         """
         left, right = 0, len(s) - 1
-
-        while left < right:
-            while left < right and not s[left].isalnum():
-                left += 1  # Skip non-alphanumeric characters
-            
-            while left < right and not s[right].isalnum():
-                right -= 1  # Skip non-alphanumeric characters
-            
-            if s[left].lower() != s[right].lower():
-                return False  # Mismatch found
-            
-            left += 1
-            right -= 1
-        
-        return True  # All
-
-
-        
+        while left<right:
+            while left<right and not s[left].isalnum():
+                left+=1
+            while left<right and not s[right].isalnum():
+                right-=1
+            if s[left].lower()!=s[right].lower():
+                return False
+            left+=1
+            right-=1
+        return True
