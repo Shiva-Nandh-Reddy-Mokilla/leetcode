7d3e77247ba897717c891f47288b0d778cfb40b6
Shiva-Nandh-Reddy-Mokilla <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-03-31 17:28:30 -0400

    Python Solution
          Submission Statistics:
          Question #: 49
          Language: Python
          Runtime: 15 ms
          Runtime percentile: 96.43
          Memory: 16.112 MB
          Memory percentile: 65.44

---
+++ 49. Group Anagrams/Solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res=defaultdict(list)
+        for s in strs:
+            sorteds = ''.join(sorted(s))
+            res[sorteds].append(s)
+        return res.values()
+
+
+        
+        
