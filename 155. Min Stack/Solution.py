1e997a27d9ff3b9b29df1c6f202f1d579e63c591
Shiva <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-05-07 23:44:34 -0400

    Python3 Solution
          Submission Statistics:
          Question #: 155
          Language: Python3
          Runtime: 15 ms
          Runtime percentile: 13.70
          Memory: 21.288 MB
          Memory percentile: 90.63

--- 155. Min Stack/Solution.py
+++ 155. Min Stack/Solution.py
@@ -1,42 +1,27 @@
-class MinStack(object):
+class MinStack:
 
     def __init__(self):
         self.stack=[]
         self.minstack=[]
+        
 
-    def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
+    def push(self, val: int) -> None:
         self.stack.append(val)
         val=min(val,self.minstack[-1] if self.minstack else val)
         self.minstack.append(val)
-
         
 
-    def pop(self):
-        """
-        :rtype: None
-        """
+    def pop(self) -> None:
         self.stack.pop()
         self.minstack.pop()
         
 
-    def top(self):
-        """
-        :rtype: int
-        """
+    def top(self) -> int:
         return self.stack[-1]
         
-        
 
-    def getMin(self):
-        """
-        :rtype: int
-        """
+    def getMin(self) -> int:
         return self.minstack[-1]
-        
 
 
 # Your MinStack object will be instantiated and called as such:
