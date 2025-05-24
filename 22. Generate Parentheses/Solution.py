44b5e62c54b333ccee00de01f64027867706a6c0
Shiva <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-05-24 08:38:49 -0400

    Python3 Solution
          Submission Statistics:
          Question #: 22
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 18.164 MB
          Memory percentile: 26.07

--- 22. Generate Parentheses/Solution.py
+++ 22. Generate Parentheses/Solution.py
@@ -2,19 +2,17 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         stack=[]
         res=[]
-        def backtrack(openN,closeN):
-            if openN==closeN==n:
+        def backtrack(openn,closen):
+            if openn==closen==n:
                 res.append("".join(stack))
                 return
-            if openN<n:
+            if openn<n:
                 stack.append("(")
-                backtrack(openN+1,closeN)
+                backtrack(openn+1,closen)
                 stack.pop()
-            if closeN<openN:
+            if closen<openn:
                 stack.append(")")
-                backtrack(openN,closeN+1)
+                backtrack(openn,closen+1)
                 stack.pop()
         backtrack(0,0)
         return res
-
-                
