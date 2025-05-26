ce8a340f20a6ad1213602edde3a3dd5759ed4009
Shiva <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-05-26 12:12:20 -0400

    Python3 Solution
          Submission Statistics:
          Question #: 153
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 18.232 MB
          Memory percentile: 9.48

--- 153. Find Minimum in Rotated Sorted Array/Solution.py
+++ 153. Find Minimum in Rotated Sorted Array/Solution.py
@@ -1,6 +1,14 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        return min(nums)
+        min=nums[0]
+        i=1
+        for i in nums:
+            if i<min:
+                min=i
+            else:
+                continue
+        return min
         
+
         
         
