b1f0ec76d242ff72b0088b61c9fa9f07f4808f7c
Shiva <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-05-28 13:41:43 -0400

    Python3 Solution
          Submission Statistics:
          Question #: 567
          Language: Python3
          Runtime: 50 ms
          Runtime percentile: 37.12
          Memory: 18.016 MB
          Memory percentile: 18.80

--- 567. Permutation in String/Solution.py
+++ 567. Permutation in String/Solution.py
@@ -1,15 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        n1, n2=len(s1), len(s2)
-        if n2<n1: return False
-        freq1, freq2 = Counter(s1), Counter(s2[0:n1])
+        n1,n2=len(s1),len(s2)
+        if n1>n2:
+            return False
+        freq1,freq2=Counter(s1),Counter(s2[0:n1])
         if freq1==freq2: return True
-        l,  r = 1, n1
+        l=1
+        r=n1
         while r<n2:
             freq2[s2[l-1]]-=1
             freq2[s2[r]]+=1
-            if freq1==freq2: return True
             r+=1
             l+=1
+            if freq1==freq2:
+                return True
         return False
+        
+
+
         
