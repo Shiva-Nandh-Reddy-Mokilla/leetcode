45f95d8e2a102538b61f914f957b2921974cd573
Shiva <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-05-29 00:04:01 -0400

    Python3 Solution
          Submission Statistics:
          Question #: 2
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.956 MB
          Memory percentile: 35.80

--- 2. Add Two Numbers/Solution.py
+++ 2. Add Two Numbers/Solution.py
@@ -7,7 +7,7 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         carry=0
         sumof=0
-        dummy=ListNode(0)
+        dummy=ListNode()
         res=dummy
         while l1 or l2 or carry:
             sum=carry
@@ -17,9 +17,9 @@ class Solution:
             if l2:
                 sum+=l2.val
                 l2=l2.next
-            sumof=sum%10
+           
             carry=sum//10
-            dummy.next=ListNode(sumof)
+            dummy.next=ListNode(sum%10)
             dummy=dummy.next
           
         return res.next
