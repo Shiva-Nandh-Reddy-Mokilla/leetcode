e408cd3f3f49da284b66bc326a335add5d688c51
Shiva <67327805+Shiva-Nandh-Reddy-Mokilla@users.noreply.github.com>
2025-07-27 15:34:15 -0400

    Python3 Solution
          Submission Statistics:
          Question #: 242
          Language: Python3
          Runtime: 7 ms
          Runtime percentile: 88.99
          Memory: 17.724 MB
          Memory percentile: 86.52

--- 242. Valid Anagram/Solution.py
+++ 242. Valid Anagram/Solution.py
@@ -1,18 +1,4 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if len(s)!=len(t):
-            return False
-        counts={}
-        countt={}
-        for i in range(len(s)):
-            counts[s[i]]=1+counts.get(s[i],0)
-            countt[t[i]]=1+countt.get(t[i],0)
-        return counts==countt
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s)==Counter(t)
         
-
-
